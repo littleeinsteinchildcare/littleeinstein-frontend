f4f0ae246c10983803a08448d05d37d9c9d2831f
Matt Lafferty <matt@pdx.edu>
2025-02-24 14:08:28 -0800

    LEC-34 Frontend push demo

--- src/components/introduction/SaveName.tsx
+++ src/components/introduction/SaveName.tsx
@@ -3,7 +3,7 @@ const SaveName = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">Type Your Name Below</h1>
       <ul>
-        <li>Amrit Thapa</li>
+        <li>Matt Lafferty</li>
       </ul>
     </div>
   );
