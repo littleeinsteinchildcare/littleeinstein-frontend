e303204f1ca4bd1eb1b9fe51e50d5f7c1da1388e
andrespdx <andg2@pdx.edu>
2025-03-07 23:15:46 -0800

    added slideshow component-no slide yet

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,9 +2,9 @@ import outputonlinepngtools from "../assets/outputonlinepngtools.png";
 
 const Homepage = () => {
   return (
-    <div className="bg-[#FFFBCF] min-h-screen p-8 grid grid-flow-col gap-8">
+    <div className="bg-[#FFFBCF] min-h-screen max-w-screen p-8 grid grid-flow-col gap-8 overflow-x-hidden">
       {/*Location picture component*/}
-      <div className="bg-[#94EE8F] max-h-[452px] w-[756px] p-6 rounded-4xl flex items-center max-w-[756px]">
+      <div className="bg-[#94EE8F] max-h-[452px] w-[756px] p-6 rounded-4xl flex items-center max-w-[756px] col-start-1 col-end-3">
         <div className="p-4">
           <img
             src={outputonlinepngtools}
@@ -14,7 +14,7 @@ const Homepage = () => {
         </div>
       </div>
       {/*Side text */}
-      <div className="min-w-[550px] pt-45 pb-45">
+      <div className="pt-37 pb-35">
         <p className="text-black text-3xl font-semibold pb-5">
           1789 SE River RD Hillsboro OR 97123
         </p>
@@ -25,6 +25,8 @@ const Homepage = () => {
           <strong>Ages:</strong> 2 months to 4 years
         </p>
       </div>
+      {/*slideshow*/}
+      <div className="bg-[#94EE8F] min-h-[200px] p-6 rounded-lg col-start-1 col-end-7 items-center justify-center mx-50"></div>
     </div>
   );
 };
