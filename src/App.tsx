6ff678d6aae7fec137aba38a4a1e07047929fe8f
realamritthapa <realamritthapa@gmail.com>
2025-03-09 00:18:06 -0800

    Fixed liner issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import CalendarPage from "./pages/calendar/Calendar.tsx";
+import CalendarPage from "@/pages/calendar/Calendar.tsx";
 
 // not adding react-router-dom yet, just temp for now for PR
 // Line Below: commented to fix merge conflict.
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <div>
       <CalendarPage />
-      <!-- Line Below: commented to fix merge conflict. -->
-      <!-- <Staff /> -->
+      {/* <!-- Line Below: commented to fix merge conflict. -->
+      <!-- <Staff /> --> */}
     </div>
   );
 };
