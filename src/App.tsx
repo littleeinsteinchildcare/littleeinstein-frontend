828186e53dcb64a48809849124e86b0346ef2ae9
andrespdx <andg2@pdx.edu>
2025-03-13 09:56:00 -0700

    added homepage with the other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 //import CalendarPage from "@/pages/calendar/Calendar.tsx";
-import ResourcesPage from "./pages/resourcesPage";
+//import ResourcesPage from "./pages/resourcesPage";
 // not adding react-router-dom yet, just temp for now for PR
 // Line Below: commented to fix merge conflict.
 //import Staff from "./pages/Staff";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
     <div>
-      <ResourcesPage />
-      {/*
+      <Homepage />
+      {/*<ResourcesPage />
       <!-- Line Below: commented to fix merge conflict. -->
         <CalendarPage />
       <!-- <Staff /> --> */}
