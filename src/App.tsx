9d74df52ad8bc10b62fef7507f571d794690e6bc
andrespdx <166752734+andrespdx@users.noreply.github.com>
2025-03-14 17:49:50 -0700

    Lec 41 homepage (#14)
    * LEC-31 make a push
    
    * new branch added env/ to gitignore
    
    * fixed location picture component
    
    * Added side text, changed to grid instead of flex
    
    * added slideshow component-no slide yet
    
    * LEC-43 basic layout with mock links
    
    * LEC-43 basic layout with mock links
    
    * added env/ to .gitignore
    
    * tried adding env folder to .gitignore
    
    * Remove env/ folder from tracking
    
    * added homepage with the other pages
    
    * Changed . to @

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import CalendarPage from "@/pages/calendar/Calendar.tsx";
-
+//import CalendarPage from "@/pages/calendar/Calendar.tsx";
+//import ResourcesPage from "@/pages/resourcesPage";
 // not adding react-router-dom yet, just temp for now for PR
 // Line Below: commented to fix merge conflict.
-//import Staff from "./pages/Staff";
+//import Staff from "@/pages/Staff";
+import Homepage from "@/pages/Homepage";
 
 const App = () => {
   return (
     <div>
-      <CalendarPage />
-      {/* <!-- Line Below: commented to fix merge conflict. -->
+      <Homepage />
+      {/*<ResourcesPage />
+      <!-- Line Below: commented to fix merge conflict. -->
+        <CalendarPage />
       <!-- <Staff /> --> */}
     </div>
   );
