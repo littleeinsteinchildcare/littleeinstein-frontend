50add283aef5886696c2e3b7b2594789243ee098
Karla Martinez <karla.m5161@gmail.com>
2025-05-22 23:55:22 -0700

    footer needed update forgot to do it earlier, yelp and google links need to be varified by sponsor before they get updated but Insta and Facebook links updated.

--- src/components/footer/footerConnectLinks.tsx
+++ src/components/footer/footerConnectLinks.tsx
@@ -5,11 +5,13 @@ const FooterConnectLinks = () => {
     <ul className="space-y-4 text-sm text-gray-800 mb-4">
       <li className="flex items-center space-x-2 hover:underline">
         <FaFacebookF />
-        <a href="https://facebook.com">Facebook</a>
+        <a href="https://www.facebook.com/Littleeinsteinchildcare">Facebook</a>
       </li>
       <li className="flex items-center space-x-2 hover:underline">
         <FaInstagram />
-        <a href="https://instagram.com">Instagram</a>
+        <a href="https://www.instagram.com/littleeinsteinchildcare/">
+          Instagram
+        </a>
       </li>
       <li className="flex items-center space-x-2 hover:underline">
         <FaGoogle />
