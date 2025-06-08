05212c94f780c3ca58f390c4441b2241df2692a2
realamritthapa <realamritthapa@gmail.com>
2025-06-08 15:21:38 -0700

    both forms now fillable and stored in firestorage

--- src/pages/resources/ODEChildEnrollment.tsx
+++ src/pages/resources/ODEChildEnrollment.tsx
@@ -1,16 +1,17 @@
-import ChildEnrollmentAuthorization from "@/assets/ChildEnrollmentAuthorization.pdf";
 import { useTranslation } from "react-i18next";
 
 const ODEChildEnrollmentForm = () => {
   const { t } = useTranslation();
 
+  const pdfUrl =
+    "https://firebasestorage.googleapis.com/v0/b/little-einstein-childcare.firebasestorage.app/o/ChildEnrollmentAuthorizationFillable.pdf?alt=media&token=b41da4b3-b2ae-48e1-8778-09dc372ec12c";
   return (
     <div className="flex flex-col items-center p-10">
       <h1 className="text-2xl font-bold mb-4">
         {t("resources.ODEChildEnrollmentForm")}
       </h1>
       <iframe
-        src={ChildEnrollmentAuthorization}
+        src={pdfUrl}
         width="80%"
         height="600px"
         title="ODE Child Enrollment Authorization Form"
